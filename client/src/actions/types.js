9e1add6bd0c5760437544862d2dca2d6b074258b
Giezi <bob.giezi@gmail.com>
2021-01-05 20:51:47 +0100

    added artist and publisher reducers

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,5 +1,20 @@
+// Items
 export const GET_ITEMS = 'GET_ITEMS';
 export const GET_ITEM = 'GET_ITEM';
 export const ADD_ITEM = 'ADD_ITEM';
 export const EDIT_ITEM = 'EDIT_ITEM';
 export const DELETE_ITEM = 'DELETE_ITEM';
+
+// Artists
+export const GET_ARTISTS = 'GET_ARTISTS';
+export const GET_ARTIST = 'GET_ARTIST';
+export const ADD_ARTIST = 'ADD_ARTIST';
+export const EDIT_ARTIST = 'EDIT_ARTIST';
+export const DELETE_ARTIST = 'EDIT_ARTIST';
+
+// Publisher
+export const GET_PUBLISHERS = 'GET_PUBLISHERS';
+export const GET_PUBLISHER = 'GET_PUBLISHER';
+export const ADD_PUBLISHER = 'ADD_PUBLISHER';
+export const EDIT_PUBLISHER = 'EDIT_PUBLISHER';
+export const DELETE_PUBLISHER = 'EDIT_PUBLISHER';
